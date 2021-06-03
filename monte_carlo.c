ca00da122b70c3a43025df433045a3ce7abe641f
Joel0432 <85241838+Joel0432@users.noreply.github.com>
2021-06-04 04:12:07 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,22 +38,21 @@ int main(void) {
     }
   }
 }
+
+float mc_pi(int x)
+{	//printf("\niter=%d",x);
+	float xco,yco,pi;
+	int incir=0;
+	for(int i=0;i<x;i++)
+	{
+		xco=frandom();
+		yco=frandom();
+		//printf("x=%f\ty=%f\n",xco,yco);
+		float dist = sqrt(pow(xco,2)+pow(yco,2));
+		if(dist<=1.0)
+			incir++;
+	}
+	pi=4*((float)incir/(float)x);
+	//printf("\nincir=%d\tpi=%f",incir,pi);
+return pi;
 }
-float mc_pi(int n)
-{
- float num=0.0,x,y;
- for(int i=1;i<=n;i++)
- {
-  x=frandom();
-  y=frandom();
-  if(sqrt((x*x)+(y*y))<=1) 
-  {
-	num++;
-  }
- }
-  return 4.0*(num/(float)n);
-}
-
-
-
-
